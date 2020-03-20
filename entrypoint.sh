1dddf25d626dfe24db0368bd583c23309a0f5174
sirrkitt <33910211+sirrkitt@users.noreply.github.com>
2020-03-20 07:23:49 -0700

    Update entrypoint.sh
    Fixed paste errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 /usr/sbin/unitd --control unix:/var/run/control.unit.sock
 
-while [ ! -S /var/run/control.unit.sock ]; do echo "$0: Waiting for control socket to be created..."; /bin/sleep 0.1;                                                                                                                         done
+while [ ! -S /var/run/control.unit.sock ]; do echo "$0: Waiting for control socket to be created..."; /bin/sleep 0.1; done
 /usr/bin/curl -s -X GET --unix-socket /var/run/control.unit.sock http://localhost/
 
 curl -X PUT --data-binary @/config.json --unix-socket /var/run/control.unit.sock http://localhost/config
 kill -TERM `/bin/cat /var/run/unit.pid`
-while [ -S /var/run/control.unit.sock ]; do echo "$0: Waiting for control socket to be removed..."; /bin/sleep 0.1; d                                                                                                                        one
+while [ -S /var/run/control.unit.sock ]; do echo "$0: Waiting for control socket to be removed..."; /bin/sleep 0.1; done
 
 if [ ! -e /config/config.php ]
 then
